c881295af3347a8bdeef49ead19e94704b1284b8
Seokwon SONG <softcontext@gmail.com>
2017-11-03 20:38:33 +0900

    Q:Child data to Parent.Solved

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -13,11 +13,33 @@ class App extends React.Component {
     this.refs.xx.value = '';
   }
 
+  receiveData = (data) => {
+    alert(data);
+  }
+
   render() {
     return (
       <div>
         <input ref="xx"></input>
         <button onClick={this.reset}>reset</button>
+        <hr/>
+        <Child receiveData={this.receiveData} />
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component {
+  sendToParent = () => {
+    // 부모에게 받은 함수를 사용하여 부모에게 자식의 데이터를 전달한다.
+    this.props.receiveData(this.refs.yy.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <input ref="yy"></input>
+        <button onClick={this.sendToParent}>send to parent</button>
       </div>
     );
   }
